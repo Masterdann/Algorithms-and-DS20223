42c0ff4f5b8dc22027b1d229e105640f7a9ac684
Masterdann <52262288+Masterdann@users.noreply.github.com>
2023-04-08 19:01:23 +0300

    Create solution.cpp

---
+++ Reverse linked list/solution.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+struct node
+{
+    char data;
+    struct node* next;
+    node(char  data)
+    {
+        this->data = data;
+        next = NULL;
+    }
+};
+struct lilist
+{
+    node* head;
+    lilist()
+    {
+        head = NULL;
+    }
+    void print()
+    {
+        struct node* puntero = head;
+        while (puntero != NULL)
+        {
+            std::cout << puntero->data;
+            puntero = puntero->next;
+        }
+    }
+    void push(char data)
+    {
+        node* puntero = new node(data);
+        puntero->next = head;
+        head = puntero;
+    }
+};
+int main()
+{
+    std::string s;
+    while(getline(std::cin, s))
+    {
+        lilist lista;
+        int n=s.size();
+        for(int i=0; i<n; i++)
+        {
+            lista.push(s[i]);
+        }
+        lista.print();
+        std::cout<<"\n";
+    }
+}
