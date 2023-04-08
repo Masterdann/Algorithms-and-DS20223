53688d473fc5a48fa9f916fcce486ec2771aef2e
Masterdann <52262288+Masterdann@users.noreply.github.com>
2023-04-08 19:03:11 +0300

    Create solution.cpp

---
+++ Binary search on the segment/solution.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int v[100030];
+bool bs(int l, int r, int t){
+    r--;
+    while(l<r){
+        int mid=(l+r)/2;
+        if(v[mid]<t)l=mid+1;
+        else r=mid;
+    }
+    return t==v[l];
+}
+int main()
+{
+    int n;
+    while(std::cin>>n){
+        for(int i=0;i<n;i++)std::cin>>v[i];
+        int q;
+        std::cin>>q;
+        while(q--){
+            int l, r, t;
+            std::cin>>l>>r>>t;
+            if(bs( l,  r ,  t))std::cout<<"YES"<<"\n";
+            else std::cout<<"NO"<<"\n";
+        }
+    }
+}
