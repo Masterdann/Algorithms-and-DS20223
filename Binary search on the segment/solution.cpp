9c0fe6e00abfedc9af05d7d36888c1e61e95ff53
Masterdann <52262288+Masterdann@users.noreply.github.com>
2023-04-09 23:08:27 +0300

    Update solution.cpp

--- Binary search on the segment/solution.cpp
+++ Binary search on the segment/solution.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
-
 int v[100030];
-bool bs(int l, int r, int t){
+bool bs(int l, int r, int t)
+{
     r--;
-    while(l<r){
+    while(l<r)
+    {
         int mid=(l+r)/2;
         if(v[mid]<t)l=mid+1;
         else r=mid;
@@ -13,11 +14,13 @@ bool bs(int l, int r, int t){
 int main()
 {
     int n;
-    while(std::cin>>n){
-        for(int i=0;i<n;i++)std::cin>>v[i];
+    while(std::cin>>n)
+    {
+        for(int i=0; i<n; i++)std::cin>>v[i];
         int q;
         std::cin>>q;
-        while(q--){
+        while(q--)
+        {
             int l, r, t;
             std::cin>>l>>r>>t;
             if(bs( l,  r ,  t))std::cout<<"YES"<<"\n";
