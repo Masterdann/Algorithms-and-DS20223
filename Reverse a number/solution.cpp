5f00a8a82dcf509b7a533a561caa8dfe645092fa
Masterdann <52262288+Masterdann@users.noreply.github.com>
2023-05-27 00:08:00 +0300

    Update solution.cpp

--- Reverse a number/solution.cpp
+++ Reverse a number/solution.cpp
@@ -1,17 +1,14 @@
 #include <iostream>
 
-int main()
-{
-    unsigned long long int n;
-    while(std::cin>>n)
-    {
-        unsigned long long int m=0;
-        while(n!=0)
-        {
-            m*=10;
-            m+=(n%10);
-            n/=10;
-        }
-        std::cout<<m<<std::endl;
+int main() {
+  unsigned long long int n;
+  while (std::cin >> n) {
+    unsigned long long int m = 0;
+    while (n != 0) {
+      m *= 10;
+      m += (n % 10);
+      n /= 10;
     }
+    std::cout << m << std::endl;
+  }
 }
