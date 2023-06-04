12c4501c54507e583fb8047aba15af9d52ec41a7
Masterdann <52262288+Masterdann@users.noreply.github.com>
2023-06-04 21:53:23 +0300

    Update solution.cpp
    https://contest.yandex.ru/contest/47619/run-report/87952729/

--- Reverse linked list/solution.cpp
+++ Reverse linked list/solution.cpp
@@ -1,40 +1,40 @@
 #include <iostream>
 
 struct node {
-  char data;
-  struct node* next;
-  node(char data) {
-    this->data = data;
-    next = NULL;
-  }
+    char data;
+    struct node* next;
+    node(char data) {
+        this->data = data;
+        next = NULL;
+    }
 };
 struct lilist {
-  node* head;
-  lilist() { head = NULL; }
-  void print() {
-    struct node* puntero = head;
-    while (puntero != NULL) {
-      std::cout << puntero->data;
-      puntero = puntero->next;
+    node* head;
+    lilist() {
+        head = NULL;
     }
-  }
-  void push(char data) {
-    node* puntero = new node(data);
-    puntero->next = head;
-    head = puntero;
-  }
+    void print() {
+        struct node* puntero = head;
+        while (puntero != NULL) {
+            std::cout << puntero->data;
+            puntero = puntero->next;
+        }
+    }
+    void push(char data) {
+        node* puntero = new node(data);
+        puntero->next = head;
+        head = puntero;
+    }
 };
-int main()
-
-{
-  std::string s;
-  while (getline(std::cin, s)) {
-    lilist lista;
-    int n = s.size();
-    for (int i = 0; i < n; i++) {
-      lista.push(s[i]);
+int main() {
+    std::string s;
+    while (getline(std::cin, s)) {
+        lilist lista;
+        int n = s.size();
+        for (int i = 0; i < n; i++) {
+            lista.push(s[i]);
+        }
+        lista.print();
+        std::cout << "\n";
     }
-    lista.print();
-    std::cout << "\n";
-  }
 }
