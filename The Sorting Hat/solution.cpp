431a645a641fecdf8c134b9a8a61497a66537304
Masterdann <52262288+Masterdann@users.noreply.github.com>
2023-06-08 13:12:29 +0300

    Update solution.cpp
    https://contest.yandex.ru/contest/47445/run-report/88040452/

--- The Sorting Hat/solution.cpp
+++ The Sorting Hat/solution.cpp
@@ -1,79 +1,133 @@
-#include <iostream>
-int v[200400];
-int tree[810000];
-void init(int node, int a, int b) {
-  if (a == b) {
-    tree[node] = v[a];
-    return;
-  }
-  init(2 * node + 1, a, (a + b) / 2);
-  init(2 * node + 2, (a + b) / 2 + 1, b);
-  tree[node] = std::min(tree[2 * node + 1], tree[2 * node + 2]);
-}
-int query(int node, int a, int b, int p, int q) {
-  if (q < a || b < p) return 1e9 + 10;
-  if (p <= a && b <= q) return tree[node];
-  return std::min(query(2 * node + 1, a, (a + b) / 2, p, q),
-                  query(2 * node + 2, (a + b) / 2 + 1, b, p, q));
-}
 
-void update(int node, int a, int b, int p, int val) {
-  if (p < a || b < p) return;
-  if (a == b) {
-    tree[node] = val;
-    return;
+#include <iostream>
+#include <stack>
+using std::stack;
+template <class T>
+class MinStack {
+ public:
+  void Push(const T& value) {
+    min_stack_.emplace(value,
+                       min_stack_.empty()
+                           ? value
+                           : std::min(value, min_stack_.top().min_value));
   }
-  update(2 * node + 1, a, (a + b) / 2, p, val);
-  update(2 * node + 2, (a + b) / 2 + 1, b, p, val);
-  tree[node] = std::min(tree[2 * node + 1], tree[2 * node + 2]);
-}
+  T GetMin() { return min_stack_.top().min_value; }
+  void Pop() { min_stack_.pop(); }
+  bool Empty() { return min_stack_.empty(); }
+  T Top() { return min_stack_.top().value; }
+  void Clear() {
+    while (!min_stack_.empty()) {
+      min_stack_.pop();
+    }
+  }
+  size_t Size() { return min_stack_.size(); }
+
+ private:
+  struct Pair {
+    T value;
+    T min_value;
+    Pair(T value, T min_value) {
+      this->value = value;
+      this->min_value = min_value;
+    }
+  };
+  stack<Pair> min_stack_;
+};
+template <class T>
+struct Queue {
+  MinStack<T> s1, s2;
+  T size;
+  T first;
+  Queue() { size = 0; }
+  void Enqueue(T data) {
+    s1.Push(data);
+    if (size == 0) {
+      first = data;
+    }
+
+    size++;
+  }
+  T Dequeue() {
+    if (s1.Empty() && s2.Empty()) {
+      throw "error";
+    }
+    if (s2.Empty()) {
+      while (!s1.Empty()) {
+        s2.Push(s1.Top());
+        s1.Pop();
+      }
+    }
+    int x = s2.Top();
+    s2.Pop();
+    if (!s2.Empty()) {
+      first = s2.Top();
+    }
+    size--;
+    return x;
+  }
+  T Front() {
+    if (s1.Empty() && s2.Empty()) {
+      throw "error";
+    }
+    if (s2.Empty()) {
+      while (!s1.Empty()) {
+        s2.Push(s1.Top());
+        s1.Pop();
+      }
+    }
+    return s2.Top();
+  }
+  int Size() { return size; }
+  void Clear() {
+    if (!s1.Empty()) {
+      s1.Clear();
+    }
+    if (!s2.Empty()) {
+      s2.Clear();
+    }
+    size = 0;
+  }
+  T Min() {
+    if (s1.Empty() && s2.Empty()) {
+      throw "error";
+    }
+    if (s1.Empty()) {
+      return s2.GetMin();
+    }
+    if (s2.Empty()) {
+      return s1.GetMin();
+    }
+    return std::min(s1.GetMin(), s2.GetMin());
+  }
+  ~Queue() {}
+};
 int main() {
+  Queue<int> q1 = Queue<int>();
   int n;
-  for (int i = 0; i < 200002; i++) v[i] = 1e9 + 10;
-  init(0, 0, 200001);
-  int l = 0, r = 0;
   std::cin >> n;
-  while (n--) {
-    std::string s;
+  std::string s;
+  for (int i = 0; i < n; i++) {
     std::cin >> s;
-    if (s == "enqueue") {
-      std::cout << "ok";
-      int x;
-      std::cin >> x;
-      v[r] = x;
-      update(0, 0, 200001, r, x);
-      r++;
-    }
-    if (s == "front") {
-      if (l == r)
-        std::cout << "error";
-      else
-        std::cout << v[l];
-    }
-    if (s == "dequeue") {
-      if (l == r)
-        std::cout << "error";
-      else {
-        std::cout << v[l];
-        v[l] = -1;
-        l++;
+    try {
+      if (s == "enqueue") {
+        int elem;
+        std::cin >> elem;
+        q1.Enqueue(elem);
+        std::cout << "ok" << std::endl;
+      } else if (s == "front") {
+        std::cout << q1.Front() << std::endl;
+      } else if (s == "dequeue") {
+        std::cout << q1.Dequeue() << std::endl;
+      } else if (s == "size") {
+        std::cout << q1.Size() << std::endl;
+      } else if (s == "clear") {
+        q1.Clear();
+        std::cout << "ok" << std::endl;
+      } else if (s == "min") {
+        std::cout << q1.Min() << std::endl;
       }
+    } catch (...) {
+      std::cout << "error" << std::endl;
     }
-    if (s == "size") {
-      std::cout << r - l;
-    }
-    if (s == "clear") {
-      std::cout << "ok";
-      l = r;
-    }
-    if (s == "min") {
-      if (l == r)
-        std::cout << "error";
-      else {
-        int answer = query(0, 0, 200001, l, r - 1);
-        std::cout << answer;
-      }
-    }
-    std::cout << "\n";
   }
 }
